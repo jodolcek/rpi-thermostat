b01c6fb8f053265876b4452993601987944693cd
jodolcek <josip.dolcek@gmail.com>
2026-05-31 20:53:24 +0200

    Dodana klasa

---
+++ android/app/src/main/java/hr/fipu/termostat/Informations.java
@@ -0,0 +1,22 @@
+package hr.fipu.termostat;
+
+public class Informations {
+    private String Heating;
+    private Float Temperature;
+    private Float SetPoint;
+    Informations(String heating, float temperature, float setPoint) {
+        this.Heating = heating;
+        this.Temperature = temperature;
+        this.SetPoint = setPoint;
+    }
+    public String getHeating() {
+        return this.Heating;
+    }
+    public float getTemperature() {
+        return this.Temperature;
+    }
+    public float getSetPoint() {
+        return this.SetPoint;
+    }
+
+}
