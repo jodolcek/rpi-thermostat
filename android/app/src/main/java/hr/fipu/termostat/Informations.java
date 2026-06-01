0d30ba05b59e2064e650bcb5ad26cd224cb689fa
jodolcek <josip.dolcek@gmail.com>
2026-06-01 20:11:31 +0200

    Dodan WebSocket

--- android/app/src/main/java/hr/fipu/termostat/Informations.java
+++ android/app/src/main/java/hr/fipu/termostat/Informations.java
@@ -1,18 +1,20 @@
 package hr.fipu.termostat;
 
 public class Informations {
-    private String Heating;
-    private Float Temperature;
-    private Float SetPoint;
+    public String Heating;
+    public Float Temperature;
+    public Float SetPoint;
     Informations(String heating, float temperature, float setPoint) {
         this.Heating = heating;
         this.Temperature = temperature;
         this.SetPoint = setPoint;
     }
     public String getHeating() {
+
         return this.Heating;
     }
     public float getTemperature() {
+
         return this.Temperature;
     }
     public float getSetPoint() {
--- android/app/src/main/java/hr/fipu/termostat/MainActivity.java
+++ android/app/src/main/java/hr/fipu/termostat/MainActivity.java
@@ -1,6 +1,9 @@
 package hr.fipu.termostat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+public Informations informations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);return insets;
         });
+        TextView temp, point, heating;
+
+        temp = findViewById(R.id.CurrentTemp);
+        point = findViewById(R.id.SetTempText);
+        heating = findViewById(R.id.HeatingStatus);
+        WebSocket ws = new WebSocket();
+
+        ws.connect();
+        ws.setCallback(info -> {
+            runOnUiThread(() -> {
+                temp.setText(String.valueOf(info.getTemperature()));
+                point.setText(String.valueOf(info.getSetPoint()));
+                if ("off".equals(info.getHeating())) {
+                    heating.setText("Grijanje isključeno");
+                    heating.setTextColor(Color.RED);
+                } else {
+                    heating.setText("Grijanje uključeno");
+                    heating.setTextColor(Color.GREEN);
+                }
+            });
+        });
+
+
+
     }
+
 }
